c9ca13d1737405ecc03b9769727e99c5efc20d6d
正晴 鶴田 <mtsuruda@jot.co.jp>
2021-06-09 17:58:51 +0900

    荷主運賃決定マスタ追加

--- BASEDLL/PrevDispMap.vb
+++ BASEDLL/PrevDispMap.vb
@@ -276,6 +276,14 @@ Public Module PrevDispMap
         ''' </summary>
         Public Const MC0012 As String = "ASP.GR_GRMC0012MODEL_ASPX"
         ''' <summary>
+        ''' 荷主運賃決定マスタ（条件）
+        ''' </summary>
+        Public Const MC0013S As String = "ASP.GR_GRMC0013SELECT_ASPX"
+        ''' <summary>
+        ''' 荷主運賃決定マスタ（実行）
+        ''' </summary>
+        Public Const MC0013 As String = "ASP.GR_GRMC0013UNTINKETEI_ASPX"
+        ''' <summary>
         ''' 品名マスタ（条件）
         ''' </summary>
         Public Const MD0001S As String = "ASP.GR_GRMD0001SELECT_ASPX"
